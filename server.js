76873c07a14bff7533d7e267751e6b5041c432b2
rudrasalokhe <rudrasalokhe100@gmail.com>
2025-06-26 23:51:29 +0530

    database work

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-const engineerRoute = require('../product management/routes/engineer.js')
-
-app.use('/api/v1/engineer', )
+const engineerRouter = require('../product management/routes/engineer.js');
+const managerRouter = require('../product management/routes/manager.js');
+const tasksRouter = require('../product management/routes/tasks.js');
+app.use(express.json());
+app.use('/api/v1/engineer',engineerRouter);
+app.use('/api/v1/manager', managerRouter);
+app.use('/api/v1/tasks', tasksRouter);
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Mongodb connected!!");
+    app.listen(3000, ()=>{
+        console.log("Server running on PORT 3000");
+    })
+}).catch(err)
+    console.log("error connecting to the database", err);
